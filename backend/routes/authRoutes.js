9888ec43fa8abf71d2c9a8eb22bffecd66c8b674
Tanishka Sharma <anjanasharma8448766174@gmail.com>
2025-06-19 23:15:29 +0530

    done

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,19 +1,21 @@
 const router = require('express').Router();
 const passport = require('passport');
-const generateToken = require("../utils/generateToken")
+const generateToken = require("../utils/generateToken");
 
-// Google Login 
-router.get('/google',passport.authenticate('google',{scope:['profile','email']}));
+// Google Login
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 // Google callback
-router.get("/google/callback", passport.authenticate("google", {
-  session: false,
-  failureRedirect: "/login",
-}), (req, res) => {
-  const token = generateToken(req.user);
-  // Send JWT token in a redirect or cookie or response
-  res.redirect(`https://t-edu-ai.vercel.app?token=${token}`);
-});
+router.get("/google/callback",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "/login", 
+  }),
+  (req, res) => {
+    const token = generateToken(req.user); 
+    
+    res.redirect(`https://t-edu-ai.vercel.app/login?token=${token}`);
+  }
+);
 
-
-module.exports = router
+module.exports = router;
